9ad1f87057884832f07494162d37c880aa339336
Mike Pearce <mike@dubdubdubdude.com>
2025-08-28 16:47:24 +1000

    Footer backend and GQL setup for footer fields

--- wp-content/themes/WESTBOURNE2025/Components/NavigationFooter/functions.php
+++ wp-content/themes/WESTBOURNE2025/Components/NavigationFooter/functions.php
@@ -34,64 +34,57 @@ Options::addTranslatable('NavigationFooter', [
         'endpoint' => 0
     ],
     [
-        'label' => 'Information Card',
-        'name' => 'infoCard',
+        'label' => 'Questions Card',
+        'name' => 'questionsCard',
         'type' => 'group',
         'sub_fields' => [
             [
-                'label' => 'Title',
-                'name' => 'title',
+                'label' => 'Text',
+                'name' => 'text',
                 'type' => 'text',
-                'default_value' => 'Need some more information?',
+                'default_value' => 'Have questions? We’ve gathered answers to the most common queries to help you find the information you need quickly and easily.',
             ],
             FieldVariables\getCTA(),
         ]
     ],
     [
-        'label' => 'Contact Card',
-        'name' => 'contactCard',
+        'label' => 'Card A',
+        'name' => 'cardA',
         'type' => 'group',
         'sub_fields' => [
             [
                 'label' => 'Title',
                 'name' => 'title',
                 'type' => 'text',
-                'default_value' => 'Want to speak with someone?',
+                'default_value' => 'Begin Your Journey at Westbourne',
+            ],
+             [
+                'label' => 'Text',
+                'name' => 'text',
+                'type' => 'text',
+                'default_value' => 'We are excited to announce we have been awarded the Educator‘s 5-Star Employer of Choice Award 2024.',
             ],
             FieldVariables\getCTA(),
-            [
-                'label' => 'Phone Number',
-                'name' => 'phoneNumber',
-                'type' => 'text',
-                'default_value' => '1300 783 435',
-            ],
         ]
     ],
     [
-        'label' => 'Acknowledgement Text',
-        'name' => 'contentHtml',
-        'type' => 'wysiwyg',
-        'media_upload' => 1,
-        'required' => 1,
-        'toolbar' => 'basic',
-        'default_value' => 'Uniting AgeWell acknowledges the Traditional Custodians of the lands on which we live, work and provide services. We pay our respects to their Elders, past, present and emerging.',
-    ],
-    [
-        'label' => 'Newsletter',
-        'name' => 'newsletter',
+        'label' => 'Card B',
+        'name' => 'cardB',
         'type' => 'group',
         'sub_fields' => [
             [
                 'label' => 'Title',
                 'name' => 'title',
                 'type' => 'text',
-                'default_value' => 'Subscribe now and stay updated with Uniting AgeWell',
+                'default_value' => 'Book a tour and see Westbourne in action',
             ],
-            [
-                'label' => 'Form Shortcode',
-                'name' => 'formShortcode',
+             [
+                'label' => 'Text',
+                'name' => 'text',
                 'type' => 'text',
+                'default_value' => 'We are excited to announce we have been awarded the Educator‘s 5-Star Employer of Choice Award 2024.',
             ],
+            FieldVariables\getCTA(),
         ]
     ],
     [
@@ -120,7 +113,83 @@ Options::addTranslatable('NavigationFooter', [
         'label' => 'Copyright Text',
         'name' => 'copyright',
         'type' => 'text',
-        'default_value' => 'Copyright © 2025 Uniting AgeWell Limited ABN xxxxxxxxxxxxxxx All rights reserved.',
+        'default_value' => 'All rights reserved. © 2021 Westbourne Grammar School',
+    ],
+    [
+        'label' => 'Link 1',
+        'name' => 'link1',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => 'Link Text',
+                'name' => 'linkText',
+                'type' => 'text',
+                'default_value' => 'CRICOS Provider No.00355F',
+            ],
+             [
+                'label' => 'URL',
+                'name' => 'link',
+                'type' => 'text',
+                'default_value' => '#',
+            ],
+        ]
+    ],
+    [
+        'label' => 'Link 2',
+        'name' => 'link2',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => 'Link Text',
+                'name' => 'linkText',
+                'type' => 'text',
+                'default_value' => 'Privacy Policy',
+            ],
+             [
+                'label' => 'URL',
+                'name' => 'link',
+                'type' => 'text',
+                'default_value' => '#',
+            ],
+        ]
+    ],
+    [
+        'label' => 'Link 3',
+        'name' => 'link3',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => 'Link Text',
+                'name' => 'linkText',
+                'type' => 'text',
+                'default_value' => 'Child Safe School',
+            ],
+             [
+                'label' => 'URL',
+                'name' => 'link',
+                'type' => 'text',
+                'default_value' => '#',
+            ],
+        ]
+    ],
+     [
+        'label' => 'Link 4',
+        'name' => 'link4',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => 'Link Text',
+                'name' => 'linkText',
+                'type' => 'text',
+                'default_value' => 'National Redress Scheme',
+            ],
+             [
+                'label' => 'URL',
+                'name' => 'link',
+                'type' => 'text',
+                'default_value' => '#',
+            ],
+        ]
     ],
     [
         'label' => __('Labels', 'flynt'),
@@ -168,3 +237,229 @@ Options::addTranslatable('NavigationFooter', [
     ],
 
 ]);
+
+add_action('graphql_register_types', function() {
+    // CTA object type (used in multiple cards)
+    register_graphql_object_type('CTA', [
+        'description' => __('Call to Action', 'flynt'),
+        'fields' => [
+//            'label' => [
+//                'type' => 'String',
+//                'description' => __('CTA label', 'flynt'),
+//            ],
+            'title' => [
+                'type' => 'String',
+                'description' => __('CTA label', 'flynt'),
+            ],
+            'url' => [
+                'type' => 'String',
+                'description' => __('CTA URL', 'flynt'),
+            ],
+            'target' => [
+                'type' => 'String',
+                'description' => __('CTA target', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // QuestionsCard group
+    register_graphql_object_type('QuestionsCard', [
+        'description' => __('Questions Card group', 'flynt'),
+        'fields' => [
+            'text' => [
+                'type' => 'String',
+                'description' => __('Questions Card text', 'flynt'),
+            ],
+            'cta' => [
+                'type' => 'CTA',
+                'description' => __('CTA', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // CardA and CardB group
+    register_graphql_object_type('Card', [
+        'description' => __('Card group', 'flynt'),
+        'fields' => [
+            'title' => [
+                'type' => 'String',
+                'description' => __('Card title', 'flynt'),
+            ],
+            'text' => [
+                'type' => 'String',
+                'description' => __('Card text', 'flynt'),
+            ],
+            'cta' => [
+                'type' => 'CTA',
+                'description' => __('CTA', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // SocialLinks group
+    register_graphql_object_type('SocialLinks', [
+        'description' => __('Social Links group', 'flynt'),
+        'fields' => [
+            'linkedin' => [
+                'type' => 'String',
+                'description' => __('LinkedIn URL', 'flynt'),
+            ],
+            'facebook' => [
+                'type' => 'String',
+                'description' => __('Facebook URL', 'flynt'),
+            ],
+            'instagram' => [
+                'type' => 'String',
+                'description' => __('Instagram URL', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // Link group (used for link1-link4)
+    register_graphql_object_type('FooterLink', [
+        'description' => __('Footer Link group', 'flynt'),
+        'fields' => [
+            'linkText' => [
+                'type' => 'String',
+                'description' => __('Link text', 'flynt'),
+            ],
+            'link' => [
+                'type' => 'String',
+                'description' => __('Link URL', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // Labels group
+    register_graphql_object_type('FooterLabels', [
+        'description' => __('Footer Labels group', 'flynt'),
+        'fields' => [
+            'ariaLabel' => [
+                'type' => 'String',
+                'description' => __('Navigation Aria Label', 'flynt'),
+            ],
+            'socialAriaLabel' => [
+                'type' => 'String',
+                'description' => __('Social Links Aria Label', 'flynt'),
+            ],
+            'legalAriaLabel' => [
+                'type' => 'String',
+                'description' => __('Legal Link Aria Label', 'flynt'),
+            ],
+        ],
+    ]);
+
+    // Main NavigationFooter type
+    register_graphql_object_type('NavigationFooter', [
+        'description' => __('Navigation Footer options', 'flynt'),
+        'fields' => [
+            'questionsCard' => [
+                'type' => 'QuestionsCard',
+                'description' => __('Questions Card group', 'flynt'),
+            ],
+            'cardA' => [
+                'type' => 'Card',
+                'description' => __('Card A group', 'flynt'),
+            ],
+            'cardB' => [
+                'type' => 'Card',
+                'description' => __('Card B group', 'flynt'),
+            ],
+            'copyright' => [
+                'type' => 'String',
+                'description' => __('Copyright text', 'flynt'),
+            ],
+            'link1' => [
+                'type' => 'FooterLink',
+                'description' => __('Link 1 group', 'flynt'),
+            ],
+            'link2' => [
+                'type' => 'FooterLink',
+                'description' => __('Link 2 group', 'flynt'),
+            ],
+            'link3' => [
+                'type' => 'FooterLink',
+                'description' => __('Link 3 group', 'flynt'),
+            ],
+            'link4' => [
+                'type' => 'FooterLink',
+                'description' => __('Link 4 group', 'flynt'),
+            ],
+            'socialLinks' => [
+                'type' => 'SocialLinks',
+                'description' => __('Social links group', 'flynt'),
+            ],
+            'labels' => [
+                'type' => 'FooterLabels',
+                'description' => __('Labels group', 'flynt'),
+            ],
+        ],
+    ]);
+
+    register_graphql_field('RootQuery', 'navigationFooter', [
+        'type' => 'NavigationFooter',
+        'description' => __('Navigation Footer options', 'flynt'),
+        'resolve' => function() {
+    return [
+        'questionsCard' => [
+            'text' => get_option('options_translatable_NavigationFooter_questionsCard_text'),
+            'cta' => maybe_unserialize(get_option('options_translatable_NavigationFooter_questionsCard_cta_link')),
+//            'cta' => [
+//                'label' => get_option('options_translatable_NavigationFooter_questionsCard_cta_label'),
+//                'url' => maybe_unserialize( get_option('options_translatable_NavigationFooter_questionsCard_cta_link') ),
+//                'target' => get_option('options_translatable_NavigationFooter_questionsCard_cta_target'),
+//            ],
+        ],
+        'cardA' => [
+            'title' => get_option('options_translatable_NavigationFooter_cardA_title'),
+            'text' => get_option('options_translatable_NavigationFooter_cardA_text'),
+            'cta' => maybe_unserialize(get_option('options_translatable_NavigationFooter_cardA_cta_link')),
+//            'cta' => [
+//                'label' => get_option('options_translatable_NavigationFooter_cardA_cta_label'),
+//                'url' => get_option('options_translatable_NavigationFooter_cardA_cta_url'),
+//                'target' => get_option('options_translatable_NavigationFooter_cardA_cta_target'),
+//            ],
+        ],
+        'cardB' => [
+            'title' => get_option('options_translatable_NavigationFooter_cardB_title'),
+            'text' => get_option('options_translatable_NavigationFooter_cardB_text'),
+            'cta' => maybe_unserialize(get_option('options_translatable_NavigationFooter_cardB_cta_link')),
+//            'cta' => [
+//                'label' => get_option('options_translatable_NavigationFooter_cardB_cta_label'),
+//                'url' => get_option('options_translatable_NavigationFooter_cardB_cta_url'),
+//                'target' => get_option('options_translatable_NavigationFooter_cardB_cta_target'),
+//            ],
+        ],
+        'link1' => [
+            'linkText' => get_option('options_translatable_NavigationFooter_link1_linkText'),
+            'link' => get_option('options_translatable_NavigationFooter_link1_link'),
+        ],
+        'link2' => [
+            'linkText' => get_option('options_translatable_NavigationFooter_link2_linkText'),
+            'link' => get_option('options_translatable_NavigationFooter_link2_link'),
+        ],
+        'link3' => [
+            'linkText' => get_option('options_translatable_NavigationFooter_link3_linkText'),
+            'link' => get_option('options_translatable_NavigationFooter_link3_link'),
+        ],
+        'link4' => [
+            'linkText' => get_option('options_translatable_NavigationFooter_link4_linkText'),
+            'link' => get_option('options_translatable_NavigationFooter_link4_link'),
+        ],
+        'socialLinks' => [
+            'linkedin' => get_option('options_translatable_NavigationFooter_socialLinks_linkedin'),
+            'facebook' => get_option('options_translatable_NavigationFooter_socialLinks_facebook'),
+            'instagram' => get_option('options_translatable_NavigationFooter_socialLinks_instagram'),
+        ],
+        'labels' => [
+            'ariaLabel' => get_option('options_translatable_NavigationFooter_labels_ariaLabel'),
+            'socialAriaLabel' => get_option('options_translatable_NavigationFooter_labels_socialAriaLabel'),
+            'legalAriaLabel' => get_option('options_translatable_NavigationFooter_labels_legalAriaLabel'),
+        ],
+        'copyright' => get_option('options_translatable_NavigationFooter_copyright'),
+    ];
+}
+    ]);
+
+    error_log(print_r(get_option('options_translatable_NavigationFooter_cardA'), true));
+});
