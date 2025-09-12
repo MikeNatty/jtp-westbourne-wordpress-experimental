90cd1245ed3133e14349aeab57ccc3fb927d0a02
Mike Pearce <mike@dubdubdubdude.com>
2025-09-12 14:57:41 +1000

    WIP PromoBanner

---
+++ wp-content/themes/WESTBOURNE2025/Components/BlockPromoBanner/functions.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Flynt\Components\BlockPromoBanner;
+
+use Flynt\FieldVariables;
+use Flynt\Utils\Options;
+
+add_filter('Flynt/addComponentData?name=BlockPromoBanner', function (array $data): array {
+    return $data;
+});
+
+function getACFLayout()
+{
+    return [
+        'name' => 'blockPromoBanner',
+        'label' => __('Promo Banner', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => __('Title', 'flynt'),
+                'name' => 'title',
+                'type' => 'textarea',
+                'rows' => 1,
+                'new_lines' => 'br',
+                'required' => 1,
+            ],
+            [
+                'label' => __('Sub Title', 'flynt'),
+                'name' => 'subtitle',
+                'type' => 'textarea',
+                'rows' => 2,
+                'new_lines' => 'br',
+                'required' => 0,
+                // // TODO remove for prod
+                // 'default_value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            ],
+            [
+                'label' => __('Image Main', 'flynt'),
+                'instructions' => __('Image-Format: JPG, PNG, WebP. <br>Min size: 1104px for RHS image. 2880px for bg image.', 'flynt'),
+                'name' => 'image',
+                'type' => 'image',
+                'preview_size' => 'medium',
+                'mime_types' => 'jpg,jpeg,png,svg,webp',
+                'required' => 0,
+                // // TODO remove for prod
+                // 'default_value' => 91
+            ],
+            [
+                'label' => __('Button Link', 'flynt'),
+                'name' => 'link',
+                'type' => 'link',
+                'return_format' => 'array',
+                'required' => 0,
+                'wrapper' => [
+                    'width' => '50',
+                ],
+            ],
+//          FieldVariables\getCTA(),
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            FieldVariables\getAnchorOptions(),
+        ]
+    ];
+}
+
+
+// Options::addTranslatable('blockHeading', [
+//     [
+//         'label' => __('Labels', 'flynt'),
+//         'name' => 'labelsTab',
+//         'type' => 'tab',
+//         'placement' => 'top',
+//         'endpoint' => 0
+//     ],
+// ]);
